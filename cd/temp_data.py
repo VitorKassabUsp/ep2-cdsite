8c2724c0f6de0db530b0f35e0eb726dfa5e315d2
Vitor Aun Kassab <vitor.kassab@usp.br>
2023-11-19 13:20:35 -0300

    Adicionar git ignore

---
+++ cd/temp_data.py
@@ -0,0 +1,61 @@
+album_data = [{
+    "id": "1",
+    "name": "Thriller",
+    "artist": "Michael Jackson",
+    "release_year": "1982",
+    "cover_url": "https://example.com/thriller-cover.jpg"
+}, {
+    "id": "2",
+    "name": "The Dark Side of the Moon",
+    "artist": "Pink Floyd",
+    "release_year": "1973",
+    "cover_url": "https://example.com/dark-side-cover.jpg"
+}, {
+    "id": "3",
+    "name": "Abbey Road",
+    "artist": "The Beatles",
+    "release_year": "1969",
+    "cover_url": "https://example.com/abbey-road-cover.jpg"
+}, {
+    "id": "4",
+    "name": "Back in Black",
+    "artist": "AC/DC",
+    "release_year": "1980",
+    "cover_url": "https://example.com/back-in-black-cover.jpg"
+}, {
+    "id": "5",
+    "name": "Rumours",
+    "artist": "Fleetwood Mac",
+    "release_year": "1977",
+    "cover_url": "https://example.com/rumours-cover.jpg"
+}, {
+    "id": "6",
+    "name": "The Wall",
+    "artist": "Pink Floyd",
+    "release_year": "1979",
+    "cover_url": "https://example.com/the-wall-cover.jpg"
+}, {
+    "id": "7",
+    "name": "Back to Black",
+    "artist": "Amy Winehouse",
+    "release_year": "2006",
+    "cover_url": "https://example.com/back-to-black-cover.jpg"
+}, {
+    "id": "8",
+    "name": "A Night at the Opera",
+    "artist": "Queen",
+    "release_year": "1975",
+    "cover_url": "https://example.com/a-night-at-the-opera-cover.jpg"
+}, {
+    "id": "9",
+    "name": "Kind of Blue",
+    "artist": "Miles Davis",
+    "release_year": "1959",
+    "cover_url": "https://example.com/kind-of-blue-cover.jpg"
+}, {
+    "id": "10",
+    "name": "Hotel California",
+    "artist": "Eagles",
+    "release_year": "1976",
+    "cover_url": "https://example.com/hotel-california-cover.jpg"
+}]
